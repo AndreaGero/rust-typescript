c5cce1697456eb1e675de814e3954bf753ea8e5f
Andrea Gerolimetto <andreagerolimetto92@gmail.com>
2023-05-21 13:58:55 +0200

    Add Traits and Struct interactions

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
-use crate::shapes::{circle::Circle, rect::Rect};
+use crate::shapes::{circle::Circle, collisions::Collidable, rect::Rect};
 
 mod shapes;
 
 fn main() {
     let rect = Rect::default();
+    let rect2 = Rect::default();
+
     let circ = Circle::default();
+    let circ2 = Circle::default();
+
+    rect.collide(&rect2);
+    circ.collide(&circ2);
+    rect.collide(&circ);
 
     println!("{}", rect);
     println!("{}", circ);
